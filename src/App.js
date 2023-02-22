a908dbf422b5a9da691ddad39a3c7ff97a7397be
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-02-22 19:36:54 -0300

    Pruebo archivos JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import saludar from './saludar.js';
+
+const miNombre = () => 'Nahuel Lorenzo'
+const estilos = {
+  color: "red",
+  marginTop: '20px'
+}
 
 function App() {
+  
+  saludar()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola soy Nahuel Lorenzo
+        <p style={estilos}>
+          Hola soy {miNombre()}
         </p>
         <a
           className="App-link"
