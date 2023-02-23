2e433704157a99fe4efe1e5e45b091038de1e434
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-02-23 03:20:52 -0300

    agrego navbar

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import saludar from './saludar.js';
-
-const miNombre = () => 'Nahuel Lorenzo'
-const estilos = {
-  color: "red",
-  marginTop: '20px'
-}
+import Example from './components/NavBar/Navbar.js';
 
 function App() {
   
-  saludar()
+  
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={estilos}>
-          Hola soy {miNombre()}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Example></Example>
+      
+    </section>
   );
 }
 
--- src/saludar.js
+++
@@ -1,5 +0,0 @@
-function saludar(){
-    console.log("Hola mundo")
-}
-
-export default saludar
