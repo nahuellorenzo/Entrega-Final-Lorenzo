99ca6d8908de7781024d6a9d319560ea1c617881
Nahuel Lorenzo <nahuelorenzo@gmail.com>
2023-03-15 20:10:03 -0300

    agrego MOCK_DATA

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,43 @@
 import './ItemListContainer.scss'
+import { useEffect, useState } from 'react'
+import { pedirDatos } from '../../helpers/pedirDatos'
+
 
 export const ItemListContainer = ( {greeting} ) => {
 
+    const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+
+        pedirDatos()
+            .then((response) => {
+                setProductos(response)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    })
+
     return(
         <div className="containerList">
-            <h2 className='h2__containerList'>{greeting}</h2>
-            <hr></hr>
+            {
+                loading 
+                    ? <h2>Cargando...</h2> 
+                    : <div><h2 className='h2__containerList'>{greeting}</h2><hr></hr>
+                        { productos.map((producto) => {
+                            return (
+                            <div key={producto.id}>
+                                <h3>{producto.nombre}</h3>
+                                <img src={producto.img} alt={producto.nombre}></img>
+                                <p>{producto.precio}</p>
+                            </div>)
+                        }) }
+                    </div>
+            }
         </div>
     )
 }
